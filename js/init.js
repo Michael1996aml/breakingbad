5ded1f736029e49de4e196a3067947676fe1f443
Michael1996aml <Michael1996aml@gmail.com>
2020-12-03 22:46:58 -0300

    Inicio de trabajo
    Se consumio la api se creo el card y se mostro en la pagina

---
+++ js/init.js
@@ -0,0 +1,21 @@
+
+
+window.mostrar = (personajes)=>{
+    const molde = document.querySelector(".molde-per");
+    const contenedor = document.querySelector(".contenedor");
+    for(let i=0; i < personajes.length; ++i){
+        let p = personajes[i];
+        let copia = molde.cloneNode(true);
+        copia.querySelector(".nombre-per").innerText = p.name;
+        copia.querySelector(".img-per").src = p.img;
+        contenedor.appendChild(copia);
+    }
+
+
+};
+
+window.addEventListener('DOMContentLoaded',async()=>{
+    let respuesta = await axios.get("https://www.breakingbadapi.com/api/characters");
+    let personajes = respuesta.data;
+    window.mostrar(personajes);
+});
